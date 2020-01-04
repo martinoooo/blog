7967f6d6a9dde50e0ad02e6bfb72a9f5bcd14fab
xiaomeng <xiaomeng@youzan.com>
2020-01-04 19:43:22 +0800

    feat: update

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -5,10 +5,11 @@ import fs = require('fs');
 // tslint:disable-next-line: no-var-requires
 const axios = require('axios');
 import { Base64 } from 'js-base64';
+import { ACCESS_TOKEN } from '../../config';
 
 const BASE = 'https://api.github.com';
 const GET_CONTENT = BASE + '/repos/martinoooo/blog/contents/';
-const END = '?access_token=9a33207f3d90e4f6f16fad55a721b1021958fcfe';
+const END = `?access_token=${ACCESS_TOKEN}`;
 
 @Injectable()
 export class CatsService {
