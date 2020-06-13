6d8f00a02f37d912528c98956738427e25074b9a
xiaomeng <xiaomeng@youzan.com>
2020-06-13 16:15:07 +0800

    更新

--- client/src/pages/qqspace/index.tsx
+++ client/src/pages/qqspace/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-export const Hello = () => <h1>TEMP Nothing here</h1>;
+export const Hello = () => (
+  <h1 style={{ textAlign: 'center' }}>👷‍♀️ 施工中 👷</h1>
+);
 
 ReactDOM.render(<Hello />, document.getElementById('app'));
